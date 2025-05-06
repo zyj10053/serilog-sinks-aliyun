53c73c91c51f596430e3d12cdc1317d8dbe5ebf7
zyj <1005300416@qq.com>
2025-05-06 15:39:26 +0800

    【修改】Serilog.Sinks.Aliyun.csproj

--- Serilog.Sinks.Aliyun/publish.ps1
+++ Serilog.Sinks.Aliyun/publish.ps1
@@ -1,2 +1,2 @@
 cd bin/Release
-dotnet nuget push Serilog.Sinks.Aliyun.Core.1.0.1.nupkg --api-key oy2avkizpy5agimvrr4qdumfpkaggpsehi4notvs7bd6pe --source https://api.nuget.org/v3/index.json
+dotnet nuget push Serilog.Sinks.Aliyun.Core.1.0.103.nupkg --api-key oy2avkizpy5agimvrr4qdumfpkaggpsehi4notvs7bd6pe --source https://api.nuget.org/v3/index.json
